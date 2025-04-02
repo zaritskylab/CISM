08f69fbf90e041a6ea61bc472327283696c2d434
Barak Milshtein <milshtbarak@gmail.com>
2025-04-02 10:31:00 +0300

    cleaned imports and some other small things in the auxiliary files

--- auxiliary/plugin_utils.py
+++ auxiliary/plugin_utils.py
@@ -9,7 +9,7 @@ import matplotlib.cm as cm
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 
 motif = {
-    ('A', 'B'): {}, 
+    ('A', 'B'): {},
     ('A', 'C'): {},
     ('B', 'C'): {},
     'A': {'type': 9},
@@ -17,32 +17,36 @@ motif = {
     'C': {'type': 11}
 }
 
+
 def rotate_point(points, angle_degrees):
     angle_rad = np.radians(angle_degrees)
     rotation_matrix = np.array([
         [np.cos(angle_rad), -np.sin(angle_rad)],
         [np.sin(angle_rad), np.cos(angle_rad)]
     ])
-    
+
     # Check if points is a single point or a list of points
     if isinstance(points[0], (int, float)):
         return np.dot(rotation_matrix, points)
     else:
         return [np.dot(rotation_matrix, point) for point in points]
 
-def fig_to_np_array(fig):
 
+def fig_to_np_array(fig):
     canvas = FigureCanvasAgg(fig)
     canvas.draw()
-    
+
     image_data = np.frombuffer(canvas.tostring_rgb(), dtype=np.uint8)
     return image_data.reshape(canvas.get_width_height()[::-1] + (3,))
+
 
 def convert_dict(input_dict):
     new_dict = {}
     for key, value in input_dict.items():
         new_dict[value['name']] = key
     return new_dict
+
+
 def parse_motif_input(input_str, cell_type_dict):
     # Convert cell type names to indices
     type_to_index = convert_dict(cell_type_dict)
@@ -77,13 +81,15 @@ def parse_motif_input(input_str, cell_type_dict):
         motifs.append(motif)
 
     return motifs
+
+
 def parse_motif_input2(input_str, cell_type_dict):
     # Convert cell type names to indices
     type_to_index = convert_dict(cell_type_dict)
 
     lines = input_str.strip().split("\n")
     motif = {}
-    
+
     for line in lines:
         line = line.strip()
         if "->" in line:
@@ -102,13 +108,14 @@ def parse_motif_input2(input_str, cell_type_dict):
                 motif[node] = {"type": type_to_index[type_val]}
             else:
                 raise ValueError(f"Unknown cell type: {type_val}")
-    
+
     return motif
+
 
 def parse_motif_input1(input_str):
     lines = input_str.strip().split("\n")
     motif = {}
-    
+
     for line in lines:
         line = line.strip()
         if "->" in line:
@@ -122,30 +129,31 @@ def parse_motif_input1(input_str):
         elif ".type =" in line:
             node, type_val = map(str.strip, line.split(".type ="))
             motif[node] = {"type": int(type_val.strip('"'))}
-    
+
     return motif
 
-def create_tagged_image(cell_data, image, mapper_dict, patinet_number, fov = None):
-        # Load the CSV data
-        if fov is None:
-            cell_data_specific_p = cell_data[cell_data[mapper_dict['patints_col']] == patinet_number]
-        else:
-            cell_data_specific_p = cell_data[(cell_data[mapper_dict['patints_col']] == patinet_number) \
-                                                  & (cell_data[mapper_dict['fov_col']] == fov)]
 
-        segmented_image_array = np.array(image)
-        # Create a mapping from cell type to integer
-        cell_types = cell_data_specific_p[mapper_dict['cell_types_col']].unique()
-        cell_type_to_int = {cell_type: idx + 1 for idx, cell_type in enumerate(cell_types)}
+def create_tagged_image(cell_data, image, mapper_dict, patinet_number, fov=None):
+    # Load the CSV data
+    if fov is None:
+        cell_data_specific_p = cell_data[cell_data[mapper_dict['patints_col']] == patinet_number]
+    else:
+        cell_data_specific_p = cell_data[(cell_data[mapper_dict['patints_col']] == patinet_number) \
+                                         & (cell_data[mapper_dict['fov_col']] == fov)]
 
-        # Create a new image array for cell types
-        cell_type_image_array = np.zeros_like(segmented_image_array)
-        # Populate the new image array based on cell type mapping
-        for _, row in cell_data_specific_p.iterrows():
-            cell_label = row[mapper_dict['cell_index_col']]
-            cell_type_int = cell_type_to_int[row[mapper_dict['cell_types_col']]]
-            cell_type_image_array[segmented_image_array == cell_label] = cell_type_int
-        return cell_type_image_array.astype(np.uint8)
+    segmented_image_array = np.array(image)
+    # Create a mapping from cell type to integer
+    cell_types = cell_data_specific_p[mapper_dict['cell_types_col']].unique()
+    cell_type_to_int = {cell_type: idx + 1 for idx, cell_type in enumerate(cell_types)}
+
+    # Create a new image array for cell types
+    cell_type_image_array = np.zeros_like(segmented_image_array)
+    # Populate the new image array based on cell type mapping
+    for _, row in cell_data_specific_p.iterrows():
+        cell_label = row[mapper_dict['cell_index_col']]
+        cell_type_int = cell_type_to_int[row[mapper_dict['cell_types_col']]]
+        cell_type_image_array[segmented_image_array == cell_label] = cell_type_int
+    return cell_type_image_array.astype(np.uint8)
 
 
 def find_motifs(G_full, motifs):
@@ -154,7 +162,7 @@ def find_motifs(G_full, motifs):
 
     for motif in motifs:
         motif_graph = nx.Graph()
-        
+
         # Adding edges and nodes based on motif input
         for key, value in motif.items():
             if isinstance(key, tuple):  # Edges
@@ -177,11 +185,9 @@ def find_motifs(G_full, motifs):
     return all_subgraphs
 
 
-
-
 def find_motifs1(G_full, motif):
     motif_graph = nx.Graph()
-    
+
     # Adding edges and nodes based on motif input
     for key, value in motif.items():
         if isinstance(key, tuple):  # Edges
@@ -204,28 +210,27 @@ def find_motifs1(G_full, motif):
 
 
 def generate_cell_type_structure_from_tagged(mapper):
-        num_unique_types = len(mapper)
-        
-        colormap = cm.get_cmap('tab20c', num_unique_types)
-        colors = [colormap(i) for i in range(num_unique_types)]
-        
-        # Background and Unknown are hardcoded
-        cell_type_structure = {
-            0: {'name': 'Background', 'color': 'black'},
-            #1: {'name': 'Unknown', 'color': 'black'}
-        }
-        
-        # Populate the dictionary with unique cell types and their colors
-        for i, cell_type in enumerate(mapper.keys(), start=1):
-            cell_type_structure[cell_type] = {'name': mapper[cell_type], 'color': colors[i-1]}
-            
-        return cell_type_structure
+    num_unique_types = len(mapper)
+
+    colormap = cm.get_cmap('tab20c', num_unique_types)
+    colors = [colormap(i) for i in range(num_unique_types)]
+
+    # Background and Unknown are hardcoded
+    cell_type_structure = {
+        0: {'name': 'Background', 'color': 'black'},
+        # 1: {'name': 'Unknown', 'color': 'black'}
+    }
+
+    # Populate the dictionary with unique cell types and their colors
+    for i, cell_type in enumerate(mapper.keys(), start=1):
+        cell_type_structure[cell_type] = {'name': mapper[cell_type], 'color': colors[i - 1]}
+
+    return cell_type_structure
 
 
 # Load the image
 def build_graph(image, list_of_cells_to_exclude=[]):
-    
-    image_full = image#[500:1500, 500:1500]
+    image_full = image  #[500:1500, 500:1500]
     image_original = copy.deepcopy(image_full)
     # Preprocess the image based on the provided code
     #image_full[image_full == 4] = 17
@@ -233,7 +238,7 @@ def build_graph(image, list_of_cells_to_exclude=[]):
     #image_full = np.where(image_full > 3, image_full - 1, image_full)
     if len(list_of_cells_to_exclude) > 0:
         image_full = np.where(np.isin(image_full, list_of_cells_to_exclude), 50, image_full)
-    exc = 50 #if len(list_of_cells_to_exclude) > None0 else 
+    exc = 50  #if len(list_of_cells_to_exclude) > None0 else
     # Extract cell contours and centroid coordinates
     cnts_full = cv2.findContours(image_full, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts_full = cnts_full[0] if len(cnts_full) == 2 else cnts_full[1]
@@ -246,7 +251,7 @@ def build_graph(image, list_of_cells_to_exclude=[]):
         coords_full.append([int(x), int(y)])
         if image_full[int(y), int(x)] == exc:
             point2cell_full[idx] = exc
-            p2c[idx] = image_original[int(y), int(x)]#0
+            p2c[idx] = image_original[int(y), int(x)]  #0
         else:
             point2cell_full[idx] = image_full[int(y), int(x)]
             p2c[idx] = image_original[int(y), int(x)]
@@ -256,12 +261,12 @@ def build_graph(image, list_of_cells_to_exclude=[]):
     # Generate edges and node features
     edges_full = []
     node_ft_full = []
-    
+
     for i in range(len(coords_full)):
         if point2cell_full[i] == exc:
             continue
         else:
-            i_neigh = neighbours_full[indptr_neigh_full[i]:indptr_neigh_full[i+1]]
+            i_neigh = neighbours_full[indptr_neigh_full[i]:indptr_neigh_full[i + 1]]
             node_ft_full.append(point2cell_full[i])
             for cell in i_neigh:
                 if point2cell_full[cell] == exc:
@@ -270,13 +275,12 @@ def build_graph(image, list_of_cells_to_exclude=[]):
                 edges_full.append(pair)
     edges_full = np.asarray(edges_full).T
 
-
     G_full = nx.Graph()
     for left, right in edges_full.T:
         # Add nodes with their cell types
         G_full.add_node(left, cell_type=point2cell_full[left])
         G_full.add_node(right, cell_type=point2cell_full[right])
-        
+
         # Add the edge
         G_full.add_edge(left, right)
 
@@ -286,9 +290,11 @@ def build_graph(image, list_of_cells_to_exclude=[]):
 
     return image_original, G_full, coords_full, point2cell_full, p2c
 
+
 # Define the node matcher function
 def node_matcher(node1, node2):
     return node1['cell_type'] == node2['type']
+
 
 # Convert the motif dictionary into a graph structure
 #def find_motifs(G_full, motif = motif):
@@ -298,14 +304,12 @@ def node_matcher(node1, node2):
 #        if isinstance(attr, dict) and 'type' in attr:
 #            motif_graph_8.add_node(node, type=attr['type'])
 
-    # Search for the motif in the graph
+# Search for the motif in the graph
 #    subgraphs_8 = nx.algorithms.isomorphism.GraphMatcher(G_full, motif_graph_8, node_match=node_matcher).subgraph_isomorphisms_iter()
 
-    # Convert subgraphs to a list and filter
+# Convert subgraphs to a list and filter
 #    subgraphs_8_list = list(subgraphs_8)
 #    return subgraphs_8_list
-
-
 
 
 def vis_graph_and_motifs(coords_full,
@@ -321,17 +325,16 @@ def vis_graph_and_motifs(coords_full,
     # Visualize the corrected graph overlay
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8))
 
-    
-    
     # Display the image with Voronoi overlay
     colors = ['black'] + [cell_types[key]['color'] for key in cell_types.keys()]
-    custom_cmap = ListedColormap(colors)   
-    print(custom_cmap) 
+    custom_cmap = ListedColormap(colors)
+    print(custom_cmap)
     ax1.imshow(image_original, cmap=custom_cmap, origin='upper')
 
     colors = []
     for cell_type, attributes in cell_types.items():
-        ax2.plot([], [], 'o', color=attributes['color'], label=f"Cell Type {attributes['name']} ({cell_type})", markersize=10)
+        ax2.plot([], [], 'o', color=attributes['color'], label=f"Cell Type {attributes['name']} ({cell_type})",
+                 markersize=10)
     ax2.legend(loc="upper left")
     ax2.axis('off')
     #voronoi_plot_2d(vor_full, ax=ax, show_vertices=False, line_colors='black', line_width=0.5, line_alpha=0.6, point_size=2)
@@ -341,9 +344,10 @@ def vis_graph_and_motifs(coords_full,
 
     motif_nodes = [node for subgraph in subgraphs_8_list for node in subgraph.keys()]
     node_colors = ['white' if node in motif_nodes else 'none' for node in G_full.nodes()]
-    nx.draw_networkx(G_full, pos=corrected_pos, node_size=5, with_labels=False, node_color=node_colors, edge_color='white', ax=ax1)
+    nx.draw_networkx(G_full, pos=corrected_pos, node_size=5, with_labels=False, node_color=node_colors,
+                     edge_color='white', ax=ax1)
     for sg in subgraphs_8_list:
-        bb = {v : k for k,v in sg.items()}
+        bb = {v: k for k, v in sg.items()}
         motif_nodes = [bb['A'], bb['B'], bb['C']]
         motif_edges = [(motif_nodes[i], motif_nodes[j]) for i, j in [(0, 1), (0, 2), (1, 2)]]
         nx.draw_networkx_edges(G_full, pos=corrected_pos, edgelist=motif_edges, edge_color='red', width=2.5, ax=ax1)
@@ -360,12 +364,12 @@ def generate_color_map(cell_types):
     """
     unique_cell_types = np.unique(cell_types)
     num_colors = len(unique_cell_types)
-    
+
     # Generate a color palette with as many unique colors as there are cell types
     color_palette = plt.cm.tab20.colors + plt.cm.tab20c.colors  # Combine two color palettes to get more unique colors
     colors = color_palette * (num_colors // len(color_palette) + 1)  # Repeat palette if more colors are needed
     colors = colors[:num_colors]
-    
+
     return {cell_type: colors[i] for i, cell_type in enumerate(unique_cell_types)}
 
 
@@ -375,21 +379,21 @@ def generate_cell_type_structure(cell_types):
     """
     unique_cell_types = np.unique(cell_types)
     num_unique_types = len(unique_cell_types)
-    
+
     # Generate a wide range of unique colors using a colormap
     colormap = cm.get_cmap('tab20c', num_unique_types)
     colors = [colormap(i) for i in range(num_unique_types)]
-    
+
     # Background and Unknown are hardcoded
     cell_type_structure = {
         #0: {'name': 'Background', 'color': 'black'},
         #1: {'name': 'Unknown', 'color': 'black'}
     }
-    
+
     # Populate the dictionary with unique cell types and their colors
     for i, cell_type in enumerate(unique_cell_types, start=0):
         cell_type_structure[i] = {'name': cell_type, 'color': colors[i]}
-        
+
     return cell_type_structure
 
 
@@ -401,24 +405,23 @@ def build_cell_graph(data,
                      filter_out_cell_type_cluster=None,
                      buffer=0,
                      alpha=0.01):
-
     from scipy.spatial import distance
 
     """Build a graph from cell coordinates and cell types."""
     coords = data[['centroid-0', 'centroid-1']].values
-    cell_types = data['common_cell_types'].values 
+    cell_types = data['common_cell_types'].values
     cells_idx = data['cell_id']
 
     include_indices = [i for i, ctype in enumerate(cell_types) if ctype not in exclude]
     coords_included = coords[include_indices]
-    #cell_types = cell_types[include_indices]
-    
+    # cell_types = cell_types[include_indices]
+
     idx2cell = {idx: cell_type for idx, cell_type in enumerate(cell_types)}
     cell_type_to_index = {v['name']: k for k, v in tnbc_cells_type.items()}
-    
+
     # Use the above mapping to generate the desired dictionary
     p2c = {cell_idx: cell_type_to_index[cell_type] for cell_idx, cell_type in idx2cell.items()}
-    
+
     points = np.array(coords)
     indptr_neigh, neighbours = Delaunay(points).vertex_neighbor_vertices
     edges = []
@@ -426,7 +429,7 @@ def build_cell_graph(data,
     for i, idx in enumerate(coords):
         if tnbc_cells_type[p2c[i]]['name'] in exclude:
             continue
-        i_neigh = neighbours[indptr_neigh[i]:indptr_neigh[i+1]]
+        i_neigh = neighbours[indptr_neigh[i]:indptr_neigh[i + 1]]
         for cell in i_neigh:
             if tnbc_cells_type[p2c[cell]]['name'] in exclude:
                 continue
@@ -450,7 +453,7 @@ def build_cell_graph(data,
                                     cell_type_to_index[filter_out_cell_type_cluster],
                                     buffer=buffer,
                                     alpha=alpha)
-    
+
     return G, clusters, p2c, coords, coords_included
 
 
